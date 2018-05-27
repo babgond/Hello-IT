04bbb57916198f734e0f35cca6a90cabd867e047
babgond <babgond@gmail.com>
2018-05-27 21:26:03 +0200

    Update com.github.babgond.hello-it.MDM.sh
    support des versions antérieurs a 10.13.4

--- CustomScripts/com.github.babgond.hello-it.MDM.sh
+++ CustomScripts/com.github.babgond.hello-it.MDM.sh
@@ -7,14 +7,13 @@
 #  On-click: /
 #
 #  Status:
-#    Green  - MDM Enrolled if macos >= 10.13.4
-#    Red - NO MDM Enrolled if macos >= 10.13.4
-#    grey - non test if macos < 10.13.4
+#    Green  - MDM Enrolled if macos 
+#    Red - NO MDM Enrolled if macos 
 #
 #  Created by babgond
 #
 #  Written: 27/05/2018
-#  Updated: 
+#  Updated: 27/05/2018
 #
 ### The following line load the Hello IT bash script lib
 . "$HELLO_IT_SCRIPT_SH_LIBRARY/com.github.ygini.hello-it.scriptlib.sh"
@@ -30,24 +29,33 @@ function fromCronAction {
 function setTitleAction {
 
   osversion="$(sw_vers | grep "ProductVersion" | awk '{print $2}')"
-  buildversion="$(sw_vers | grep "Build" | awk '{print $2}')"
-  if [ "$osversion" >= "10.13.4"]
+  #
+  #faire le test sur des chiffre et non sur des caracteres.
+  if [[ "$osversion">="10.13.4" ]]
 	then
  	 DEPstatus="$(profiles status -type enrollment | grep "Enrolled via DEP" | awk '{print $2}')"
 	 MDMStatus="$(profiles status -type enrollment | grep "MDM enrollment" | awk '{print $2}')"
 	 updateTitle "MDM : $MDMStatus"
-		if [ "$MDMStatus" = "Yes" OR "Yes (User Approved)"]
+		if ["$MDMStatus" = "Yes" OR "Yes (User Approved)"]
 		then
 	 	 updateState "${STATE[0]}"
 		 updateTooltip "DEP : $DEPstatus"
 		else 
 		 updateState "${STATE[2]}"
-         updateTooltip "pas de MDM installé"
+         updateTooltip "pas de MDM decteté"
 		fi
-	else	 
-     updateTitle "MDM : non testé"
-	 updateState "${STATE[3]}"
-     updateTooltip "il faut macOS 10.13.4"
+	else	
+	MDMStatus="$(profiles -C | awk '{print $1$2$3}')"
+		if [[ "$MDMStatus"="Thereareno" ]]
+		then
+    	  updateTitle "MDM : Non"
+		  updateState "${STATE[2]}"
+    	  updateTooltip "pas de MDM installé"
+    	 else
+    	 	updateTitle "MDM : Oui"
+		  updateState "${STATE[0]}"
+    	  updateTooltip "pour le DEP il faut 10.13.4"
+     	fi
 	fi
   setEnabled YES
 }
